84b31724d5c07d7bd3524daf46f67d212bc12e5d
URK1991 <umair.khan@ku.ac.ae>
2024-10-08 10:19:39 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import torch
 if __name__ == "__main__":
     print('Training Model')
     model_type = #ResNet18 or ResNet18_SA
-    tr_model, tstloss, trloss = train_model()
+    tr_model, tstloss, trloss = train_model(model_type)
     torch.save(tr_model.state_dict(), '')
